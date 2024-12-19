654c77c514d811f46fc0b6efe1ce3184fd6f88c6
Alex <alexjoelaid97@gmail.com>
2024-12-19 09:11:30 +0100

    added some Screens

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { View } from 'react-native';
 import { Text } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import SignInScreen from './Screens/SignInScreen';
 import { GeneralStyle } from './Style/ChartStyle';
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
     <SafeAreaView style={GeneralStyle.container}>
       <View style={GeneralStyle.container}>
         <Text> App start sida </Text>
+        <SignInScreen />
       </View>
     </SafeAreaView>
   );
---
+++ Screens/SignInScreen.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { View } from 'react-native';
+import { Text } from 'react-native-paper';
+
+export default function SignInScreen() {
+  return (
+    <View>
+      <Text>SIGN-IN SCREEN</Text>
+    </View>
+  );
+}
