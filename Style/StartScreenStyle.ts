3b3a6486b5724d7bebf4c37ca3d4274f5db25be5
Alex <alexjoelaid97@gmail.com>
2025-01-08 11:32:17 +0100

    formatting

--- Style/StartScreenStyle.ts
+++ Style/StartScreenStyle.ts
@@ -1,59 +1,57 @@
 import { StyleSheet } from 'react-native';
 
 export const StartScreenStyle = StyleSheet.create({
-    container : {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        overflow: 'visible',
-    },
-    headerBox: {
-        // marginTop: '10%',
-        width: '100%',
-        height: '20%',
-        backgroundColor: '#f4f5f7',
-        // justifyContent: 'center',
-        alignItems: 'center',
-        flex: 1,
-    },
-    textHeader: {
-        marginTop: '20%',
-        fontSize: 30,
-        fontWeight: 'bold',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-   itemBox: {
-        width: '100%',
-        height: '50%',
-        backgroundColor: 'white',
-        marginBottom: '15%',
-    },
-    listItems: {
-        flex: 1,
-        borderBottomWidth: 1,
-        borderBottomColor: '#d9d9d9',
-        // alignContent: 'center',
-        flexDirection: 'row',
-        alignItems: 'center',
-        // justifyContent: 'space-between',
-        height: 50,
-        marginLeft: '5%',
-        marginRight: '5%',
-   },
-    textItem: {
-        fontSize: 17,
-        marginLeft: '5%',
-        color: 'black',
-        flex: 1,
-    },
-    listItemPositionStart: {
-        justifyContent: 'flex-start',
-    },
-    listItemPositionEnd: {
-        justifyContent: 'flex-end',
-        marginRight: '1%',
-    },
-   
-   
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    overflow: 'visible',
+  },
+  headerBox: {
+    // marginTop: '10%',
+    width: '100%',
+    height: '20%',
+    backgroundColor: '#f4f5f7',
+    // justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+  textHeader: {
+    marginTop: '20%',
+    fontSize: 30,
+    fontWeight: 'bold',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  itemBox: {
+    width: '100%',
+    height: '50%',
+    backgroundColor: 'white',
+    marginBottom: '15%',
+  },
+  listItems: {
+    flex: 1,
+    borderBottomWidth: 1,
+    borderBottomColor: '#d9d9d9',
+    // alignContent: 'center',
+    flexDirection: 'row',
+    alignItems: 'center',
+    // justifyContent: 'space-between',
+    height: 50,
+    marginLeft: '5%',
+    marginRight: '5%',
+  },
+  textItem: {
+    fontSize: 17,
+    marginLeft: '5%',
+    color: 'black',
+    flex: 1,
+  },
+  listItemPositionStart: {
+    justifyContent: 'flex-start',
+  },
+  listItemPositionEnd: {
+    justifyContent: 'flex-end',
+    marginRight: '1%',
+  },
 });
