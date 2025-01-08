da2aad72bf1019b90e7dfd710b0af1780382be19
H T <henke.teesalu@gmail.com>
2025-01-08 14:02:02 +0100

    Before merge

--- Navigation/TabNavigator.tsx
+++ Navigation/TabNavigator.tsx
@@ -1,5 +1,8 @@
+import { MaterialIcons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import * as React from 'react';
+import { Pressable } from 'react-native';
+import { LogoTitle } from '../Components/Header';
 import PremiseScreen from '../Screens/PremiseScreen';
 import ReportScreen from '../Screens/ReportScreen';
 
@@ -12,7 +15,30 @@ const Tab = createBottomTabNavigator<TabParamList>();
 
 export default function TabNavigator() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ navigation }) => ({
+        headerRight: () => (
+          <Pressable onPress={() => console.log('navigating')}>
+            <MaterialIcons
+              name="exit-to-app"
+              size={30}
+              color="#D32F2F"
+            />
+          </Pressable>
+        ),
+        headerLeft: () => (
+          <Pressable onPress={() => navigation.goBack()}>
+            <MaterialIcons
+              name="arrow-back"
+              size={30}
+              color="#000"
+            />
+          </Pressable>
+        ),
+        headerTitle: () => <LogoTitle />,
+        headerTitleAlign: 'center',
+      })}
+    >
       <Tab.Screen
         name="PremiseScreen"
         component={PremiseScreen}
