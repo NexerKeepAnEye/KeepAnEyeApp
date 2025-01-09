752c062df09ec39a986043901f9152e9bd7bfc8e
H T <henke.teesalu@gmail.com>
2025-01-09 13:05:39 +0100

    Styling fondation done

--- Screens/ReportScreen.tsx
+++ Screens/ReportScreen.tsx
@@ -1,57 +1,13 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import Accordion from '../Components/Accordion';
-import { usePremiseContext } from '../PremiseState/PremiseContext';
+import PremiseCard from '../Components/PremiseCard';
 
 export default function ReportScreen() {
-  const { state } = usePremiseContext();
-  // const { premise } = state;
-
   return (
-    <View style={{ flex: 1, padding: 10 }}>
-      <Text
-        style={{
-          alignItems: 'center',
-          justifyContent: 'center',
-          fontSize: 25,
-        }}
-      >
-        REPORT SCREEN
-      </Text>
-      {state.premise?.Name ? (
-        <View>
-          <Text
-            style={{
-              alignItems: 'center',
-              justifyContent: 'center',
-              fontSize: 15,
-            }}
-          >
-            Premise Id: {state.premise.Id}
-          </Text>
-          <Text
-            style={{
-              alignItems: 'center',
-              justifyContent: 'center',
-              fontSize: 15,
-            }}
-          >
-            Designation: {state.premise.Designation}
-          </Text>
-          <Text
-            style={{
-              alignItems: 'center',
-              justifyContent: 'center',
-              fontSize: 15,
-            }}
-          >
-            Name: {state.premise.Name}
-          </Text>
-          <Accordion />
-        </View>
-      ) : (
-        <Text>No premise provided</Text>
-      )}
+    <View style={{ flex: 1, alignItems: 'center' }}>
+      <PremiseCard />
+      <Accordion />
     </View>
   );
 }
---
+++ Style/PremiseCardStyle.ts
@@ -0,0 +1,44 @@
+import { StyleSheet } from 'react-native';
+
+export const premiseCardStyle = StyleSheet.create({
+  card: {
+    margin: 10,
+    padding: 10,
+    borderRadius: 8,
+    elevation: 4, // Adds shadow for Android
+    shadowColor: '#000', // Adds shadow for iOS
+    shadowOffset: { width: 0, height: 2 }, // Adds shadow for iOS
+    shadowOpacity: 0.2, // Adds shadow for iOS
+    shadowRadius: 4, // Adds shadow for iOS
+    backgroundColor: '#ffffff',
+    minWidth: '90%',
+    minHeight: '10%',
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  iconContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  content: {
+    flex: 1,
+  },
+  title: {
+    color: '#000000',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  noTitle: {
+    color: 'red',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginLeft: '22%',
+    marginTop: '4%',
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#666',
+  },
+});
