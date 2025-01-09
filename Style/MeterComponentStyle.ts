e73d9000885112a27cb08aec9754db962ced3e52
Alex <alexjoelaid97@gmail.com>
2025-01-09 11:12:12 +0100

    prettier format

--- Style/MeterComponentStyle.ts
+++ Style/MeterComponentStyle.ts
@@ -1,37 +1,34 @@
 import { StyleSheet } from 'react-native';
 
 export const MeterComponentStyle = StyleSheet.create({
-    container: {
-        width: '100%',
-        marginTop: '12%',
-        backgroundColor: '#fff',
-    },
-    listItem: {
-        height: 60,
-        borderBottomWidth: 1,
-        borderBottomColor: '#d9d9d9',
-        alignContent: 'center',
-        flexDirection: 'row',
-        paddingHorizontal: 10,
-    },
-    iconContainer: {
-        width: '10%',
-        height: '100%',
-    },
-    textContainer: {
-        flexDirection: 'column',
-        justifyContent: 'center',
-        marginLeft: '3%'
-    },
-    textStyleName: {
-        fontSize: 17,
-        color: '#222',
-    },
-    textStyleProductCode: {
-        fontSize: 15,
-        color: '#d3d3d3',
-    },
-   
-
-
- });
+  container: {
+    width: '100%',
+    marginTop: '12%',
+    backgroundColor: '#fff',
+  },
+  listItem: {
+    height: 60,
+    borderBottomWidth: 1,
+    borderBottomColor: '#d9d9d9',
+    alignContent: 'center',
+    flexDirection: 'row',
+    paddingHorizontal: 10,
+  },
+  iconContainer: {
+    width: '10%',
+    height: '100%',
+  },
+  textContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    marginLeft: '3%',
+  },
+  textStyleName: {
+    fontSize: 17,
+    color: '#222',
+  },
+  textStyleProductCode: {
+    fontSize: 15,
+    color: '#d3d3d3',
+  },
+});
--- Style/MeterIconStyle.ts
+++ Style/MeterIconStyle.ts
@@ -1,9 +1,9 @@
 import { StyleSheet } from 'react-native';
 
 export const MeterIconStyle = StyleSheet.create({
-    icon: {
-        fontSize: 30,
-        marginTop: '35%',
-        flex: 1,
-}
+  icon: {
+    fontSize: 30,
+    marginTop: '35%',
+    flex: 1,
+  },
 });
