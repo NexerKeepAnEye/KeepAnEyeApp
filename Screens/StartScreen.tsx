6f98894771b6feeb19ff87406b2dc5b108246328
Alex <alexjoelaid97@gmail.com>
2025-01-13 09:25:47 +0100

    cleanup

--- Screens/StartScreen.tsx
+++ Screens/StartScreen.tsx
@@ -71,8 +71,6 @@ export default function StartScreen({ navigation }: Props) {
         source={NexerLogo}
         style={StartScreenStyle.footer}
       />
-
-      {/* nexer logo */}
     </View>
   );
 }
