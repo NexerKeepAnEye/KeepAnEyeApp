89fc716a7095672216714851eace1789e2dde9ef
H T <henke.teesalu@gmail.com>
2025-01-20 14:26:13 +0100

    Commit before merge

--- Components/MeterDataGrid.tsx
+++ Components/MeterDataGrid.tsx
@@ -1,78 +1,112 @@
-import React, { useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import { ScrollView, View } from 'react-native';
 import { DataTable } from 'react-native-paper';
-import { usePremiseContext } from '../PremiseState/PremiseContext';
+import { meterData } from '../MockedData/MockedMeterDataMonth';
+import {
+  FilterAction,
+  filterReducer,
+  FilterState,
+  initialState,
+} from '../PremiseState/FilterReducer';
 import { MeterDataGridStyle } from '../Style/MeterDataGridStyle';
 import { MeterData } from '../Types/Type';
-import { FetchMeterData } from './FetchMeterData';
-import TestFilter from './TestFilter'; // Ensure this path is correct
+import Filter from './Filters/Filter';
 
 type Props = {
   meterId: number;
 };
 
 export default function MeterDataGrid({ meterId }: Props) {
-  const { state } = usePremiseContext();
-  const [filteredData, setFilteredData] = useState<MeterData[]>([]);
+  // const { state } = usePremiseContext();
+  // const [filteredData, setFilteredData] = useState<MeterData[]>([]);
 
-  const handleFilter = (startDate: string | null, endDate: string | null) => {
-    if (startDate === null && endDate === null) {
-      const latestData = state.meterData
-        .filter((data) => data.MeterId === meterId)
-        .slice(-20);
-      setFilteredData(latestData);
-    } else {
-      const start = new Date(startDate || '');
-      const end = new Date(endDate || '');
-      const data = state.meterData.filter(
-        (data) =>
-          data.MeterId === meterId &&
-          new Date(data.DateTime) >= start &&
-          new Date(data.DateTime) <= end,
-      );
-      setFilteredData(data);
-    }
-  };
+  // const handleFilter = (startDate: string | null, endDate: string | null) => {
+  //   if (startDate === null && endDate === null) {
+  //     const latestData = state.meterData
+  //       .filter((data) => data.MeterId === meterId)
+  //       .slice(-20);
+  //     setFilteredData(latestData);
+  //   } else {
+  //     const start = new Date(startDate || '');
+  //     const end = new Date(endDate || '');
+  //     const data = state.meterData.filter(
+  //       (data) =>
+  //         data.MeterId === meterId &&
+  //         new Date(data.DateTime) >= start &&
+  //         new Date(data.DateTime) <= end,
+  //     );
+  //     setFilteredData(data);
+  //   }
+  // };
 
+  const [state, dispatch] = useReducer<
+    React.Reducer<FilterState, FilterAction>
+  >(filterReducer, initialState);
+
+  const [filteredResults, setFilteredResults] = useState<MeterData[]>([]);
+
+  useEffect(() => {
+    // Använd mockad data
+    dispatch({
+      type: 'SET_METER_DATA',
+      payload: meterData,
+    });
+  }, []);
+  console.log('filteredResults:', filteredResults);
   return (
-    <View style={MeterDataGridStyle.container}>
-      <TestFilter onFilter={handleFilter} />
-      <ScrollView>
-        <FetchMeterData />
-        <DataTable style={MeterDataGridStyle.gridContainer}>
-          <DataTable.Header style={MeterDataGridStyle.header}>
-            <DataTable.Title textStyle={MeterDataGridStyle.title}>
-              Datum
-            </DataTable.Title>
-            <DataTable.Title textStyle={MeterDataGridStyle.title}>
-              Värde
-            </DataTable.Title>
-            <DataTable.Title textStyle={MeterDataGridStyle.title}>
-              Kostnad
-            </DataTable.Title>
-            <DataTable.Title textStyle={MeterDataGridStyle.title}>
-              Kod
-            </DataTable.Title>
-          </DataTable.Header>
-          {filteredData.length > 0 ? (
-            filteredData.map((data, index) => (
-              <DataTable.Row
-                key={index}
-                style={MeterDataGridStyle.cell}
-              >
-                <DataTable.Cell>{data.DateTime.split('T')[0]}</DataTable.Cell>
-                <DataTable.Cell>{data.Value}</DataTable.Cell>
-                <DataTable.Cell>{data.Cost}</DataTable.Cell>
-                <DataTable.Cell>{data.Code}</DataTable.Cell>
+    <>
+      <Filter
+        filters={['dateRange']}
+        setFromDate={(date) =>
+          dispatch({ type: 'SET_FROM_DATE', payload: date })
+        }
+        setToDate={(date) => dispatch({ type: 'SET_TO_DATE', payload: date })}
+        fromDate={state.fromDate}
+        toDate={state.toDate}
+        meterData={state.meterData}
+        meterId={meterId}
+        setFilteredResults={(data) => setFilteredResults(data)}
+        buttonText={'sök'}
+      />
+      <View style={MeterDataGridStyle.container}>
+        {/* <TestFilter onFilter={handleFilter} /> */}
+        <ScrollView>
+          {/* <FetchMeterData /> */}
+          <DataTable style={MeterDataGridStyle.gridContainer}>
+            <DataTable.Header style={MeterDataGridStyle.header}>
+              <DataTable.Title textStyle={MeterDataGridStyle.title}>
+                Datum
+              </DataTable.Title>
+              <DataTable.Title textStyle={MeterDataGridStyle.title}>
+                Värde
+              </DataTable.Title>
+              <DataTable.Title textStyle={MeterDataGridStyle.title}>
+                Kostnad
+              </DataTable.Title>
+              <DataTable.Title textStyle={MeterDataGridStyle.title}>
+                Kod
+              </DataTable.Title>
+            </DataTable.Header>
+            {filteredResults.length > 0 ? (
+              filteredResults.map((data, index) => (
+                <DataTable.Row
+                  key={index}
+                  style={MeterDataGridStyle.cell}
+                >
+                  <DataTable.Cell>{data.DateTime.split('T')[0]}</DataTable.Cell>
+                  <DataTable.Cell>{data.Value}</DataTable.Cell>
+                  <DataTable.Cell>{data.Cost}</DataTable.Cell>
+                  <DataTable.Cell>{data.Code}</DataTable.Cell>
+                </DataTable.Row>
+              ))
+            ) : (
+              <DataTable.Row>
+                <DataTable.Cell>No data available</DataTable.Cell>
               </DataTable.Row>
-            ))
-          ) : (
-            <DataTable.Row>
-              <DataTable.Cell>No data available</DataTable.Cell>
-            </DataTable.Row>
-          )}
-        </DataTable>
-      </ScrollView>
-    </View>
+            )}
+          </DataTable>
+        </ScrollView>
+      </View>
+    </>
   );
 }
