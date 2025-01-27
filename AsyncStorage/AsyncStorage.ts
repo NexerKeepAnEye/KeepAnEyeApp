9c4776af38f3bc6288d507c0f34a7db7fe5bb2f4
Alex <alexjoelaid97@gmail.com>
2025-01-27 10:25:45 +0100

    format

--- AsyncStorage/AsyncStorage.ts
+++ AsyncStorage/AsyncStorage.ts
@@ -1,41 +1,41 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default class StorageService {
-    static async set(key: string, value: string): Promise<void> {
-        try {
-            await AsyncStorage.setItem(key, value);
-        } catch (error) {
-            console.error('Error setting value in AsyncStorage', error);
-        }
+  static async set(key: string, value: string): Promise<void> {
+    try {
+      await AsyncStorage.setItem(key, value);
+    } catch (error) {
+      console.error('Error setting value in AsyncStorage', error);
     }
+  }
 
-    static async get(key: string): Promise<string | null> {
-        try {
-            const value = await AsyncStorage.getItem(key);
-            return value;
-        } catch (error) {
-            console.error('Error getting value from AsyncStorage', error);
-            return null;
-        }
+  static async get(key: string): Promise<string | null> {
+    try {
+      const value = await AsyncStorage.getItem(key);
+      return value;
+    } catch (error) {
+      console.error('Error getting value from AsyncStorage', error);
+      return null;
     }
+  }
 
-    static async remove(key:string): Promise<void> {
-        try {
-            await AsyncStorage.removeItem(key);
-        } catch (error) {
-            console.error('Error removing value from AsyncStorage', error);
-        }
+  static async remove(key: string): Promise<void> {
+    try {
+      await AsyncStorage.removeItem(key);
+    } catch (error) {
+      console.error('Error removing value from AsyncStorage', error);
     }
+  }
 
-    static storeApiKey = async (apiKey: string) => {
-        await StorageService.set('API_KEY', apiKey);
-    };
+  static storeApiKey = async (apiKey: string) => {
+    await StorageService.set('API_KEY', apiKey);
+  };
 
-    static getApiKey = async (): Promise<string | null> => {
-        return await StorageService.get('API_KEY');
-    };
+  static getApiKey = async (): Promise<string | null> => {
+    return await StorageService.get('API_KEY');
+  };
 
-    static clearApiKey = async (): Promise<void> => {
-        await StorageService.remove('API_KEY');
-    }
-};
+  static clearApiKey = async (): Promise<void> => {
+    await StorageService.remove('API_KEY');
+  };
+}
