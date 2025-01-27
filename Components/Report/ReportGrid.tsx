676ffcf8c056e710a808cb8f122b48615779bff6
H T <henke.teesalu@gmail.com>
2025-01-27 11:09:36 +0100

    Filter on year works

---
+++ Components/Report/ReportGrid.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ScrollView } from 'react-native';
+import { ReportGridStyle } from '../../Style/ReportGridStyleStyle';
+import { MonthlyReport } from './Monthlyreport';
+import { YearlyReport } from './Yearlyreport';
+
+interface ReportGridProps {
+  selectedReport: string;
+  searchResults: string[];
+}
+
+export const ReportGrid = ({ selectedReport }: ReportGridProps) => {
+  return (
+    <ScrollView style={ReportGridStyle.root}>
+      {selectedReport === 'Månadsrapport' && <MonthlyReport />}
+      {selectedReport === 'Årsrapport' && <YearlyReport />}
+    </ScrollView>
+  );
+};
