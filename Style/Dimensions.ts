6b182fea4f7ac526bbc0966dd1c1db659ef113f8
Alex <alexjoelaid97@gmail.com>
2025-01-28 16:41:47 +0100

    adjusted better values for positioning

--- Style/Dimensions.ts
+++ Style/Dimensions.ts
@@ -1,3 +1,9 @@
+// import { Dimensions } from 'react-native';
+
+// //  ******************
 import { Dimensions } from 'react-native';
 
-export const { width, height } = Dimensions.get('window');
+// export const { width, height } = Dimensions.get('window');
+export const { width, height } = Dimensions.get('screen');
+export const deviceWidth = Dimensions.get('screen').width;
+export const deviceHeight = Dimensions.get('screen').height;
