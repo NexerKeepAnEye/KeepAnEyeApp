a78b39633c565cd15cf41024e011dec2f9326d26
H T <henke.teesalu@gmail.com>
2025-01-31 11:14:36 +0100

    add more categories and refactor metericons

--- Components/Filters/MeterSearch.tsx
+++ Components/Filters/MeterSearch.tsx
@@ -52,29 +52,119 @@ export function MeterSearch({ setSelectedMeter }: MeterSearchProps) {
   const sections: Section[] = [
     {
       title: 'Fjärrvärme',
-      data: meters.filter((meter) => meter.ProductId === 1),
-    },
-    {
-      title: 'Vatten',
-      data: meters.filter(
-        (meter) => meter.ProductId === 4 || meter.ProductId === 7,
-      ),
-    },
-    {
-      title: 'Fjärrkyla',
-      data: meters.filter((meter) => meter.ProductId === 2),
+      data: meters.filter((meter) => meter.ProductId === 22),
     },
     {
       title: 'El',
-      data: meters.filter((meter) => meter.ProductId === 3),
+      data: meters.filter((meter) => meter.ProductId === 23),
     },
     {
-      title: 'Olja',
-      data: meters.filter((meter) => meter.ProductId === 10),
+      title: 'Fjärrkyla',
+      data: meters.filter((meter) => meter.ProductId === 24),
+    },
+    {
+      title: 'Vatten',
+      data: meters.filter((meter) => meter.ProductId === 25),
+    },
+    {
+      title: 'Effekt',
+      data: meters.filter((meter) => meter.ProductId === 26),
+    },
+    {
+      title: 'Flöde',
+      data: meters.filter((meter) => meter.ProductId === 27),
     },
     {
       title: 'Temperatur',
-      data: meters.filter((meter) => meter.ProductId === 9),
+      data: meters.filter(
+        (meter) => meter.ProductId === 28 || meter.ProductId === 29,
+      ),
+    },
+    {
+      title: 'Volym',
+      data: meters.filter((meter) => meter.ProductId === 30),
+    },
+    {
+      title: 'Olja',
+      data: meters.filter((meter) => meter.ProductId === 31),
+    },
+    {
+      title: 'Fjärrvärme sek',
+      data: meters.filter((meter) => meter.ProductId === 32),
+    },
+    {
+      title: 'El sek',
+      data: meters.filter((meter) => meter.ProductId === 33),
+    },
+    {
+      title: 'Fjärrkyla sek',
+      data: meters.filter((meter) => meter.ProductId === 34),
+    },
+    {
+      title: 'Vatten sek',
+      data: meters.filter((meter) => meter.ProductId === 35),
+    },
+    {
+      title: 'Fjärrvärme under',
+      data: meters.filter((meter) => meter.ProductId === 36),
+    },
+    {
+      title: 'El under',
+      data: meters.filter((meter) => meter.ProductId === 37),
+    },
+    {
+      title: 'Fjärrkyla under',
+      data: meters.filter((meter) => meter.ProductId === 38),
+    },
+    {
+      title: 'Vatten under',
+      data: meters.filter((meter) => meter.ProductId === 39),
+    },
+    {
+      title: 'NN',
+      data: meters.filter((meter) => meter.ProductId === 66),
+    },
+    {
+      title: 'Fjärrkyla',
+      data: meters.filter((meter) => meter.ProductId === 67),
+    },
+    {
+      title: 'Fjärrvärme Virtuell',
+      data: meters.filter((meter) => meter.ProductId === 68),
+    },
+    {
+      title: 'El Virtuell',
+      data: meters.filter((meter) => meter.ProductId === 69),
+    },
+    {
+      title: 'Industriellt Vatten',
+      data: meters.filter((meter) => meter.ProductId === 70),
+    },
+    {
+      title: 'BSK',
+      data: meters.filter((meter) => meter.ProductId === 71),
+    },
+    {
+      title: 'Utetemperatur',
+      data: meters.filter((meter) => meter.ProductId === 72),
+    },
+    {
+      title: 'Fjärrvärme Effekt',
+      data: meters.filter((meter) => meter.ProductId === 305),
+    },
+    {
+      title: 'El Effekt',
+      data: meters.filter((meter) => meter.ProductId === 306),
+    },
+    {
+      title: 'Övrigt',
+      data: meters.filter(
+        (meter) =>
+          ![
+            22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
+            39, 66, 67, 68, 69, 70, 71, 72, 305, 306,
+          ].includes(meter.ProductId),
+      ),
     },
   ].filter((section) => section.data.length > 0);
 
