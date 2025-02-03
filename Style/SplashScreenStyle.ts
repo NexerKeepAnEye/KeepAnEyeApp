c272d1eedbe149ceef43818fce785bc471954ef4
Alex <alexjoelaid97@gmail.com>
2025-02-03 12:37:29 +0100

    formatting

--- Style/SplashScreenStyle.ts
+++ Style/SplashScreenStyle.ts
@@ -1,9 +1,9 @@
 import { StyleSheet } from 'react-native';
 
 export const SplashScreenStyle = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
