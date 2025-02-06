b034ed66857d05783e906f6d240e12e545565c55
Alex <alexjoelaid97@gmail.com>
2025-02-06 09:42:22 +0100

    accordion are now responsive

--- Style/AccordionStyle.ts
+++ Style/AccordionStyle.ts
@@ -1,10 +1,12 @@
 import { StyleSheet } from 'react-native';
-import { height, width } from './Dimensions';
+import { deviceHeight, deviceWidth } from './Dimensions';
+
 
 export const AccordionStyle = StyleSheet.create({
   container: {
-    marginTop: 3,
-    width: width * 0.9, // 90% of screen width
+    marginTop: deviceHeight * 0.01,
+    width: deviceWidth * 0.9, 
+    minHeight: deviceHeight * 0.08,
     borderWidth: 1,
     borderColor: '#d9d9d9',
     borderRadius: 10,
@@ -15,30 +17,31 @@ export const AccordionStyle = StyleSheet.create({
   content: {
     flexDirection: 'row',
     alignItems: 'center',
-    width: '100%',
-    padding: width * 0.05, // 5% of screen width
+    width: deviceWidth * 0.9,
     justifyContent: 'center',
     position: 'relative',
     fontWeight: 'bold',
   },
   header: {
     textAlign: 'center',
+    textAlignVertical: 'center',
     flex: 1,
-    fontSize: width * 0.045, // 4.5% of screen width
+    fontSize: deviceWidth * 0.045,
     fontWeight: 'bold',
+    height: deviceHeight * 0.08,
   },
   dDL: {
     position: 'absolute',
     right: -1,
   },
   listButton: {
-    width: width * 0.15, // 15% of screen width
-    height: height * 0.082, // 8.4% of screen height
+    width: deviceWidth * 0.15, 
+    height: deviceHeight * 0.08,
     backgroundColor: '#d9d9d9',
     borderTopRightRadius: 10,
   },
   listExpanded: {
-    width: '100%',
+    width: deviceWidth * 0.9,
     borderTopWidth: 1,
     borderColor: '#d9d9d9',
     borderBottomLeftRadius: 10,
@@ -48,16 +51,14 @@ export const AccordionStyle = StyleSheet.create({
     borderBottomWidth: 1,
     borderColor: '#d9d9d9',
     fontWeight: 'bold',
-    justifyContent: 'center', // Center the content horizontally
+    justifyContent: 'center', 
   },
   listText: {
-    // fontWeight: 'bold',
-    textAlign: 'center', // Center the text horizontally
+    textAlign: 'center',
   },
   headerChanged: {
     textAlign: 'center',
-    fontSize: width * 0.05, // 5% of screen width
+    fontSize: deviceWidth * 0.05, 
     fontWeight: 'bold',
-    // marginBottom: 10,
   },
 });
