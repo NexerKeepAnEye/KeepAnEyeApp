f4ede430af86c56e8ad2c94e4987ece806e29865
Alex <alexjoelaid97@gmail.com>
2025-02-10 14:55:14 +0100

    format

--- Components/GroupMeter.ts
+++ Components/GroupMeter.ts
@@ -12,35 +12,35 @@ export const groupMeters = (meters: Meter[]): Section[] => {
   return [
     {
       title: 'Fjärrvärme',
-      data: filterProductIds(meters, ['FJV','fjv']),
+      data: filterProductIds(meters, ['FJV', 'fjv']),
     },
     {
       title: 'El',
-      data: filterProductIds(meters, ['EL','El']),
+      data: filterProductIds(meters, ['EL', 'El']),
     },
     {
       title: 'Fjärrkyla',
-      data: filterProductIds(meters, ['FJK','fjk']),
+      data: filterProductIds(meters, ['FJK', 'fjk']),
     },
     {
       title: 'Vatten',
-      data: filterProductIds(meters, ['VAT','vat']),
+      data: filterProductIds(meters, ['VAT', 'vat']),
     },
     {
       title: 'Effekt',
-      data: filterProductIds(meters, ['Effekt','effekt']),  
+      data: filterProductIds(meters, ['Effekt', 'effekt']),
     },
     {
       title: 'Flöde',
-      data: filterProductIds(meters, ['Flöde','flöde']),
+      data: filterProductIds(meters, ['Flöde', 'flöde']),
     },
     {
       title: 'Temperatur',
-      data: filterProductIds(meters, ['TMP','tmp']),
+      data: filterProductIds(meters, ['TMP', 'tmp']),
     },
     {
       title: 'Volym',
-      data: filterProductIds(meters, ['VOL','vol']),
+      data: filterProductIds(meters, ['VOL', 'vol']),
     },
     {
       title: 'Olja',
@@ -48,51 +48,51 @@ export const groupMeters = (meters: Meter[]): Section[] => {
     },
     {
       title: 'Fjärrvärme sek',
-      data: filterProductIds(meters, ['FJV sek','fjv sek']),
+      data: filterProductIds(meters, ['FJV sek', 'fjv sek']),
     },
     {
       title: 'El sek',
-      data: filterProductIds(meters, ['EL sek','El sek','el sek']),
+      data: filterProductIds(meters, ['EL sek', 'El sek', 'el sek']),
     },
     {
       title: 'Fjärrkyla sek',
-      data: filterProductIds(meters, ['FJK sek','fjk sek']),
+      data: filterProductIds(meters, ['FJK sek', 'fjk sek']),
     },
     {
       title: 'Vatten sek',
-      data: filterProductIds(meters, ['VAT sek','vat sek']),
+      data: filterProductIds(meters, ['VAT sek', 'vat sek']),
     },
     {
       title: 'Fjärrvärme under',
-      data: filterProductIds(meters, ['FJV under','fjv under']),
+      data: filterProductIds(meters, ['FJV under', 'fjv under']),
     },
     {
       title: 'El under',
-      data: filterProductIds(meters, ['EL under','el under']),
+      data: filterProductIds(meters, ['EL under', 'el under']),
     },
     {
       title: 'Fjärrkyla under',
-      data: filterProductIds(meters, ['FJK under','fjk under']),
+      data: filterProductIds(meters, ['FJK under', 'fjk under']),
     },
     {
       title: 'Vatten under',
-      data: filterProductIds(meters, ['VAT under','vat under']),
+      data: filterProductIds(meters, ['VAT under', 'vat under']),
     },
     {
       title: 'NN',
-      data: filterProductIds(meters, ['NN','nn']),
+      data: filterProductIds(meters, ['NN', 'nn']),
     },
     {
       title: 'Fjärrvärme Virtuell',
-      data: filterProductIds(meters, ['FJV virtuell','fjv virtuell']),
+      data: filterProductIds(meters, ['FJV virtuell', 'fjv virtuell']),
     },
     {
       title: 'El Virtuell',
-      data: filterProductIds(meters, ['EL virtuell','el virtuell']),
+      data: filterProductIds(meters, ['EL virtuell', 'el virtuell']),
     },
     {
       title: 'Industriellt Vatten',
-      data: filterProductIds(meters, ['VAT ind','industriellt vatten']),
+      data: filterProductIds(meters, ['VAT ind', 'industriellt vatten']),
     },
     {
       title: 'BSK',
@@ -104,11 +104,11 @@ export const groupMeters = (meters: Meter[]): Section[] => {
     },
     {
       title: 'Fjärrvärme Effekt',
-      data: filterProductIds(meters, ['FJV Effekt','fjv effekt']),
+      data: filterProductIds(meters, ['FJV Effekt', 'fjv effekt']),
     },
     {
       title: 'El Effekt',
-      data: filterProductIds(meters, ['EL Effekt','el effekt']),
+      data: filterProductIds(meters, ['EL Effekt', 'el effekt']),
     },
     {
       title: 'Övrigt',
