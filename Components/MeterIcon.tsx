3612e1cd626b35d748ab79702623bbd588e8a51d
Alex <alexjoelaid97@gmail.com>
2025-02-10 13:34:52 +0100

    updated icon IDs

--- Components/MeterIcon.tsx
+++ Components/MeterIcon.tsx
@@ -8,21 +8,21 @@ export default function MeterIcon({ productId }: { productId: number }) {
     ids.includes(productId);
 
   const iconSelector = (productId: number) => {
-    if (isProductId(productId, [22, 36, 32, 68, 305])) {
+    if (isProductId(productId, [22, 25, 36, 63, 67, 305, 505])) {
       return 'local-fire-department';
-    } else if (isProductId(productId, [25, 35, 39, 70, 30])) {
+    } else if (isProductId(productId, [28, 30, 33, 35, 39, 66, 70, 426])) {
       return 'water-drop';
-    } else if (isProductId(productId, [28, 29, 72])) {
+    } else if (isProductId(productId, [31, 32, 72])) {
       return 'thermostat';
-    } else if (isProductId(productId, [23, 33, 37, 69, 306])) {
+    } else if (isProductId(productId, [23, 37, 64, 68, 306, 506])) {
       return 'bolt';
-    } else if (isProductId(productId, [24, 34, 38, 67])) {
+    } else if (isProductId(productId, [24, 27, 34, 38, 65, 69])) {
       return 'ac-unit';
-    } else if (isProductId(productId, [31])) {
+    } else if (isProductId(productId, [])) {
       return 'local-gas-station';
-    } else if (isProductId(productId, [26])) {
+    } else if (isProductId(productId, [26, 29])) {
       return 'electrical-services';
-    } else if (isProductId(productId, [27])) {
+    } else if (isProductId(productId, [0])) {
       return 'double-arrow';
     }
 
