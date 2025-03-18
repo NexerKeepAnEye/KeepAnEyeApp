710f1cdbc6d12a42034db9a3ed887df350956ad8
H T <henke.teesalu@gmail.com>
2025-03-18 13:33:21 +0100

    fixed navigation problem

--- Screens/ReportScreen.tsx
+++ Screens/ReportScreen.tsx
@@ -1,10 +1,14 @@
 import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
-import { CompositeNavigationProp } from '@react-navigation/native';
+import {
+  CompositeNavigationProp,
+  useFocusEffect,
+} from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import * as React from 'react';
-import { View } from 'react-native';
+import { BackHandler, View } from 'react-native';
 import Accordion from '../Components/Accordion';
 import PremiseCard from '../Components/PremiseCard';
+import { useFilterContext } from '../Context/FilterContext';
 import { RootStackParamList } from '../Navigation/RootStackNavigation';
 import { TabParamList } from '../Navigation/TabNavigator';
 import { ReportScreenStyle } from '../Style/ReportScreenStyle';
@@ -19,6 +23,26 @@ type Props = {
 };
 
 export default function ReportScreen({ navigation }: Props) {
+  const { state } = useFilterContext();
+  useFocusEffect(
+    React.useCallback(() => {
+      const onBackPress = () => {
+        if (state.meter.length > 0) {
+          navigation.navigate('MeterDataScreen');
+        } else {
+          navigation.navigate('MeterScreen');
+        }
+        return true;
+      };
+
+      const backHandler = BackHandler.addEventListener(
+        'hardwareBackPress',
+        onBackPress,
+      );
+
+      return () => backHandler.remove();
+    }, [navigation, state.meter]),
+  );
   return (
     <View style={ReportScreenStyle.container}>
       <PremiseCard navigation={navigation} />
