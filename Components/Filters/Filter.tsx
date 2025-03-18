1afbfac5657061cde02de137660019b670f40a3f
Alex <alexjoelaid97@gmail.com>
2025-03-18 11:07:16 +0100

    merge conflict somehow

--- Components/Filters/Filter.tsx
+++ Components/Filters/Filter.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { TouchableOpacity, View } from 'react-native';
+import { ScrollView, TouchableOpacity, View } from 'react-native';
 import {
   ActivityIndicator,
   MD2Colors,
@@ -189,7 +189,7 @@ const Filter = ({
     } catch (error) {
       setLoading(false);
       console.log('error fetching meterdata:', error);
-      showSnackbar('Fel, kunde inte hämta data');
+      showSnackbar('Fel, för stor tidsperiod');
     }
 
     setLoading(false);
@@ -199,90 +199,94 @@ const Filter = ({
   return (
     <PaperProvider>
       <View>
-        <View style={filterStyle.container}>
-          {filters.includes('meter') && setMeter && (
-            <View style={filterStyle.metersContainer}>
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={true}
+          persistentScrollbar={true}
+        >
+          <View style={filterStyle.container}>
+            {filters.includes('year') && setYear && (
+              <YearSearch
+                setSelectedYear={setYear}
+                label="År"
+              />
+            )}
+            {filters.includes('yearRange') && setYear && setYearTwo && (
+              <>
+                <YearSearch
+                  setSelectedYear={setYear}
+                  label="Från år"
+                />
+                <YearSearch
+                  setSelectedYear={setYearTwo}
+                  label="Till år"
+                />
+              </>
+            )}
+            {filters.includes('compareYears') && setYear && setYearTwo && (
+              <>
+                <YearSearch
+                  setSelectedYear={setYear}
+                  label="År"
+                />
+                <YearSearch
+                  setSelectedYear={setYearTwo}
+                  label="År"
+                />
+              </>
+            )}
+            {filters.includes('dateRange') && setFromDate && setToDate && (
+              <FromToDate
+                setFromDate={setFromDate}
+                setToDate={setToDate}
+                fromDate={fromDate ?? null}
+                toDate={toDate ?? null}
+              />
+            )}
+            {filters.includes('meter') && setMeter && (
               <MeterSearch
                 setSelectedMeter={setMeter}
                 meters={meter}
               />
-            </View>
-          )}
-          {filters.includes('year') && setYear && (
-            <YearSearch
-              setSelectedYear={setYear}
-              label="År"
-            />
-          )}
-          {filters.includes('yearRange') && setYear && setYearTwo && (
-            <>
-              <YearSearch
-                setSelectedYear={setYear}
-                label="Från år"
+            )}
+            {filters.includes('resolution') && setResolution && (
+              <Resolution setSelectedResolution={setResolution} />
+            )}
+            {filters.includes('standardAdjusted') && (
+              <StandardYearAdjusted
+                isChecked={correctedValues}
+                setIsChecked={setIsChecked}
               />
-              <YearSearch
-                setSelectedYear={setYearTwo}
-                label="Till år"
-              />
-            </>
-          )}
-          {filters.includes('compareYears') && setYear && setYearTwo && (
-            <>
-              <YearSearch
-                setSelectedYear={setYear}
-                label="År"
-              />
-              <YearSearch
-                setSelectedYear={setYearTwo}
-                label="År"
-              />
-            </>
-          )}
-          {filters.includes('dateRange') && setFromDate && setToDate && (
-            <FromToDate
-              setFromDate={setFromDate}
-              setToDate={setToDate}
-              fromDate={fromDate ?? null}
-              toDate={toDate ?? null}
-            />
-          )}
-          {filters.includes('resolution') && setResolution && (
-            <Resolution setSelectedResolution={setResolution} />
-          )}
-          {filters.includes('standardAdjusted') && (
-            <StandardYearAdjusted
-              isChecked={correctedValues}
-              setIsChecked={setIsChecked}
-            />
-          )}
-          {showButton && (
-            <View style={filterStyle.buttonContainer}>
-              <TouchableOpacity
-                onPress={handleSearch}
-                style={
-                  loading
-                    ? searchButtonStyle.disableButton
-                    : searchButtonStyle.button
-                }
-                disabled={loading}
-              >
-                {loading ? (
-                  <>
-                    <Text style={searchButtonStyle.text}>Hämtar...</Text>
-                    <ActivityIndicator
-                      animating={true}
-                      color={MD2Colors.white}
-                      size="small"
-                      style={{ paddingLeft: 19 }}
-                    />
-                  </>
-                ) : (
-                  <Text style={searchButtonStyle.text}>{buttonText}</Text>
-                )}
-              </TouchableOpacity>
-            </View>
-          )}
-        </View>
+            )}
+          </View>
+        </ScrollView>
+        {showButton && (
+          <View style={filterStyle.buttonContainer}>
+            <TouchableOpacity
+              onPress={handleSearch}
+              style={
+                loading
+                  ? searchButtonStyle.disableButton
+                  : searchButtonStyle.button
+              }
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Text style={searchButtonStyle.text}>Hämtar...</Text>
+                  <ActivityIndicator
+                    animating={true}
+                    color={MD2Colors.white}
+                    size="small"
+                    style={{ paddingLeft: 19 }}
+                  />
+                </>
+              ) : (
+                <Text style={searchButtonStyle.text}>{buttonText}</Text>
+              )}
+            </TouchableOpacity>
+          </View>
+        )}
       </View>
     </PaperProvider>
   );
